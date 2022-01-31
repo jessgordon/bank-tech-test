f686cf86bca7274ccbfc6580af13522543270859
Jessica Gordon <ketchesj@gmail.com>
2022-01-31 15:26:05 +0000

    Transaction class: initializes with a balance attribute

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,8 +1,9 @@
 class Transaction
-  attr_reader :date, :amount
+  attr_reader :date, :amount, :balance
 
-  def initialize(date, amount)
+  def initialize(date, amount, balance)
     @date = Date.parse(date)
     @amount = amount.to_f
+    @balance = balance.to_f
   end
 end
