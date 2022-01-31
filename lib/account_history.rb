b25ca908c26ec14dc348f947cad7f08167772c24
Jessica Gordon <ketchesj@gmail.com>
2022-01-31 17:15:19 +0000

    Account_History Class: view method - returns clients transactions: credit transaction

--- lib/account_history.rb
+++ lib/account_history.rb
@@ -6,6 +6,18 @@ class Account_History
   end
 
   def view()
-    'date || credit || debit || balance'
+    statement = "date || credit || debit || balance\n"
+    statement_rows = @transactions.map do |transaction|
+      statement_line = "#{transaction.date.strftime('%d/%m/%Y')} || "
+      if transaction.amount > 0
+        statement_line += "#{'%.2f' % transaction.amount} || || "
+      elsif transaction.amount < 0 
+        statement_line += "|| #{'%.2f' % (transaction.amount * -1)} || "
+      else
+        statement_line += "|| || "
+      end
+      statement_line += "#{'%.2f' % transaction.balance}"
+    end
+    statement += statement_rows.join("\n")
   end
 end
--- spec/account_history_spec.rb
+++ spec/account_history_spec.rb
@@ -1,20 +1,48 @@
 require 'account_history'
 
 describe Account_History do
+
+  let(:account_history) { Account_History.new }
+
   describe '#initialization' do
     it 'is an instance of Account_History' do
-      expect(subject).to be_an_instance_of(Account_History)
+      expect(account_history).to be_an_instance_of(Account_History)
     end
 
     it 'has a transactions attribute ready to store transactions within an array' do
-      expect(subject.transactions).to be_an_instance_of(Array)
-      expect(subject.transactions).to be_empty
+      expect(account_history.transactions).to be_an_instance_of(Array)
+      expect(account_history.transactions).to be_empty
     end
   end
 
   describe '#view()' do
+
+    let(:transaction_1) { 
+      double(
+        'transaction_1', 
+        :date => Date.parse('14/01/2023'),
+        :amount => 100.0,
+        :balance => 900.0
+      )
+    }
+
+    let(:transaction_2) { 
+      double(
+        'transaction_2', 
+        :date => Date.parse('18/01/2023'),
+        :amount => -250.0,
+        :balance => 650.0
+      )
+    }
+
     it 'returns the clients bank statement with date, credit, debit and balance headings' do
-      expect(subject.view()).to eq('date || credit || debit || balance')
+      expect(account_history.view()).to eq("date || credit || debit || balance\n")
+    end
+
+    it 'returns the clients transactions: credit transaction' do
+      account_history.transactions.push(transaction_1)
+
+      expect(account_history.view()).to eq("date || credit || debit || balance\n14/01/2023 || 100.00 || || 900.00")
     end
   end
 end
