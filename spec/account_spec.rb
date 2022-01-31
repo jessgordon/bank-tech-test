ad2b9b401e73d0e3a7600bc1f849a779f421279d
Jessica Gordon <ketchesj@gmail.com>
2022-01-31 19:43:51 +0000

    Account Class: initializes with an account balance of zero

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,5 +5,10 @@ describe Account do
     it 'is an instance of Account' do
       expect(subject).to be_an_instance_of(Account)
     end
+
+    it 'initializes the current balance attribute with a value of 0.0' do
+      expect(subject.balance).to be_an_instance_of(Float)
+      expect(subject.balance).to eq(0.0)
+    end
   end
 end
