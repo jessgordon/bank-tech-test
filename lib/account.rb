08b5733e1f89a4cea9a7fd143f0478df32fb95a7
Jessica Gordon <ketchesj@gmail.com>
2022-02-01 12:03:31 +0000

    Account Class: refactored amount guard clauses

--- lib/account.rb
+++ lib/account.rb
@@ -11,18 +11,27 @@ class Account
   end
 
   def deposit(amount)
-    raise "Amount must be a number" unless ((amount.is_a? Float) || (amount.is_a? Integer))
-    raise "Amount cannot be zero" if amount == 0
-    decimal_points = amount.to_f.to_s.split(".").last.length
-    raise "Amount cannot have more than two decimal points" if decimal_points > 2
+    amount_guard_clauses(amount)
     @balance += amount
     @history.add_transaction(Date.today, amount, @balance)
     @balance
   end
-
+  
   def withdraw(amount)
     @balance -= amount
     @history.add_transaction(Date.today, -amount, @balance)
     @balance
+  end
+  
+  private
+  
+  def amount_guard_clauses(amount)
+    raise "Amount must be a number" unless ((amount.is_a? Float) || (amount.is_a? Integer))
+    raise "Amount cannot be zero" if amount == 0
+    raise "Amount cannot have more than two decimal points" if decimal_points(amount) > 2
+  end
+
+  def decimal_points(amount)
+    amount.to_f.to_s.split(".").last.length
   end
 end
